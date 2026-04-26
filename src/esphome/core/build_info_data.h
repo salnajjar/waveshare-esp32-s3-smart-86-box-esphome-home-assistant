8b346763433376932c213033d84072d554fe0619
Seri <salnajjar@users.noreply.github.com>
2026-04-26 10:23:37 +0100

    Added diagnostic data as sensors for Home Assistant

--- src/esphome/core/build_info_data.h
+++ src/esphome/core/build_info_data.h
@@ -1,13 +1,13 @@
 #pragma once
 // Auto-generated build_info data
-#define ESPHOME_CONFIG_HASH 0xe6005d93U  // NOLINT
-#define ESPHOME_BUILD_TIME 1777190472  // NOLINT
+#define ESPHOME_CONFIG_HASH 0x0f7eb336U  // NOLINT
+#define ESPHOME_BUILD_TIME 1777195141  // NOLINT
 #define ESPHOME_COMMENT_SIZE 1  // NOLINT
 #ifdef USE_ESP8266
 #include <pgmspace.h>
-static const char ESPHOME_BUILD_TIME_STR[] PROGMEM = "2026-04-26 09:01:12 +0100";
+static const char ESPHOME_BUILD_TIME_STR[] PROGMEM = "2026-04-26 10:19:01 +0100";
 static const char ESPHOME_COMMENT_STR[] PROGMEM = "";
 #else
-static const char ESPHOME_BUILD_TIME_STR[] = "2026-04-26 09:01:12 +0100";
+static const char ESPHOME_BUILD_TIME_STR[] = "2026-04-26 10:19:01 +0100";
 static const char ESPHOME_COMMENT_STR[] = "";
 #endif
