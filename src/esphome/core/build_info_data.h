a197b6ad0cdfccbc5b77d44bedb9101f29e6a68f
Seri <salnajjar@users.noreply.github.com>
2026-04-27 19:23:57 +0100

    Performance tuning and tweaking to increase responsiveness.

--- src/esphome/core/build_info_data.h
+++ src/esphome/core/build_info_data.h
@@ -1,13 +1,13 @@
 #pragma once
 // Auto-generated build_info data
-#define ESPHOME_CONFIG_HASH 0x14a38ff3U  // NOLINT
-#define ESPHOME_BUILD_TIME 1777288045  // NOLINT
+#define ESPHOME_CONFIG_HASH 0xd15cce10U  // NOLINT
+#define ESPHOME_BUILD_TIME 1777310889  // NOLINT
 #define ESPHOME_COMMENT_SIZE 1  // NOLINT
 #ifdef USE_ESP8266
 #include <pgmspace.h>
-static const char ESPHOME_BUILD_TIME_STR[] PROGMEM = "2026-04-27 12:07:25 +0100";
+static const char ESPHOME_BUILD_TIME_STR[] PROGMEM = "2026-04-27 18:28:09 +0100";
 static const char ESPHOME_COMMENT_STR[] PROGMEM = "";
 #else
-static const char ESPHOME_BUILD_TIME_STR[] = "2026-04-27 12:07:25 +0100";
+static const char ESPHOME_BUILD_TIME_STR[] = "2026-04-27 18:28:09 +0100";
 static const char ESPHOME_COMMENT_STR[] = "";
 #endif
