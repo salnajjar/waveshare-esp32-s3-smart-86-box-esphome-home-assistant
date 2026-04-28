8bd4daa412dd198e616311183aeef10a1dbff8b1
Seri <salnajjar@users.noreply.github.com>
2026-04-28 12:07:52 +0100

    Fixed the input / output dialogue boxes to make them rounded and positioned correctly.

--- src/esphome/core/build_info_data.h
+++ src/esphome/core/build_info_data.h
@@ -1,13 +1,13 @@
 #pragma once
 // Auto-generated build_info data
-#define ESPHOME_CONFIG_HASH 0xb302084fU  // NOLINT
-#define ESPHOME_BUILD_TIME 1777372015  // NOLINT
+#define ESPHOME_CONFIG_HASH 0xd7a1a9bcU  // NOLINT
+#define ESPHOME_BUILD_TIME 1777374212  // NOLINT
 #define ESPHOME_COMMENT_SIZE 1  // NOLINT
 #ifdef USE_ESP8266
 #include <pgmspace.h>
-static const char ESPHOME_BUILD_TIME_STR[] PROGMEM = "2026-04-28 11:26:55 +0100";
+static const char ESPHOME_BUILD_TIME_STR[] PROGMEM = "2026-04-28 12:03:32 +0100";
 static const char ESPHOME_COMMENT_STR[] PROGMEM = "";
 #else
-static const char ESPHOME_BUILD_TIME_STR[] = "2026-04-28 11:26:55 +0100";
+static const char ESPHOME_BUILD_TIME_STR[] = "2026-04-28 12:03:32 +0100";
 static const char ESPHOME_COMMENT_STR[] = "";
 #endif
