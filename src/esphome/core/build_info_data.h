1b89c27329b34ed2dba53c230781c8063e3db944
Seri <salnajjar@users.noreply.github.com>
2026-05-05 19:19:38 +0100

    Fix S3 mDNS hostname

--- src/esphome/core/build_info_data.h
+++ src/esphome/core/build_info_data.h
@@ -1,13 +1,13 @@
 #pragma once
 // Auto-generated build_info data
-#define ESPHOME_CONFIG_HASH 0x75f252f7U  // NOLINT
-#define ESPHOME_BUILD_TIME 1777999653  // NOLINT
+#define ESPHOME_CONFIG_HASH 0xc6feb555U  // NOLINT
+#define ESPHOME_BUILD_TIME 1778004350  // NOLINT
 #define ESPHOME_COMMENT_SIZE 1  // NOLINT
 #ifdef USE_ESP8266
 #include <pgmspace.h>
-static const char ESPHOME_BUILD_TIME_STR[] PROGMEM = "2026-05-05 17:47:33 +0100";
+static const char ESPHOME_BUILD_TIME_STR[] PROGMEM = "2026-05-05 19:05:50 +0100";
 static const char ESPHOME_COMMENT_STR[] PROGMEM = "";
 #else
-static const char ESPHOME_BUILD_TIME_STR[] = "2026-05-05 17:47:33 +0100";
+static const char ESPHOME_BUILD_TIME_STR[] = "2026-05-05 19:05:50 +0100";
 static const char ESPHOME_COMMENT_STR[] = "";
 #endif
